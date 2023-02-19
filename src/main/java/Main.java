37cb71d395f1e016cc4ebfd35282708046fa2ea1
ElliotRipa <elliot.ripa@gmail.com>
2023-02-19 23:07:24 +0100

    Moved code to Requester.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,40 +3,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("hi :3");
 
-        String url = "https://www.speedrun.com/api/v1/users?lookup=TheEskot";
+        Map<String, String> map = Requester.getJSON("https://www.speedrun.com/api/v1/users?lookup=TheEskot");
 
-        URL obj = new URL(url);
-
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        if(con instanceof HttpURLConnection) {
-            con.setRequestProperty("User-Agent", "Mozilla/5.0");
-
-            int responseCode = con.getResponseCode();
-
-            System.out.println("'GET' request is sent to URL : " + url + "\nResponse Code: " + responseCode);
-
-            if(responseCode == 200) {
-
-
-                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuffer response = new StringBuffer();
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-                System.out.println(response.toString());
-
-            }
-
-        }
+        System.out.println(map);
 
     }
 
