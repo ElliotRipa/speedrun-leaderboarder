4b6d6f17fcd76b0436959b00fe17f123cd860997
ElliotRipa <elliot.ripa@gmail.com>
2023-02-19 23:21:03 +0100

    Removed redundant dependencies.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Map;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
 
