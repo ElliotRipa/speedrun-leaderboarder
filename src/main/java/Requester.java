74eae045f034afd13ec3ea3a595c8abb66ad2790
ElliotRipa <elliot.ripa@gmail.com>
2023-02-19 23:21:16 +0100

    Cleaned up code.

--- src/main/java/Requester.java
+++ src/main/java/Requester.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Map;
 
@@ -23,7 +22,7 @@ public class Requester {
 
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
-        if(con instanceof HttpURLConnection) {
+        if(con != null) {
             con.setRequestProperty("User-Agent", "Mozilla/5.0");
 
             int responseCode = con.getResponseCode();
@@ -35,7 +34,7 @@ public class Requester {
 
                 BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
                 String inputLine;
-                StringBuffer response = new StringBuffer();
+                StringBuilder response = new StringBuilder();
                 while ((inputLine = in.readLine()) != null) {
                     response.append(inputLine);
                 }
